b8fea17e49f99453d3eb79b2be7f7976b007152d
mira97ali <140742998+mira97ali@users.noreply.github.com>
2023-10-15 00:54:51 +0300

    Update printf_string.c

--- printf_string.c
+++ printf_string.c
@@ -1,21 +1,56 @@
 #include "main.h"
 
 /**
- * printf_string - prints a string
- * @args: number of arguements
- * @printed: the characters
- * Return: printed charcaters
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-
-int printf_string(va_list args, int printed)
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	char *string = va_arg(args, char *);
+	int length = 0, i;
+	char *str = va_arg(types, char *);
 
-	while (*string != '\0')
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
 	{
-		_putchar(*string);
-		printed++;
-		string++;
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
 	}
-	return (printed);
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
+
+	return (write(1, str, length));
 }
