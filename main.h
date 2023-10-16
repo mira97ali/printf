213f7813e1d99a14d2d631fd3938d5b07300bb92
mira97ali <140742998+mira97ali@users.noreply.github.com>
2023-10-16 23:55:14 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <limits.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 int _printf(const char *format, ...);
 int get_flags(const char *format, int *i);
