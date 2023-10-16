ac1b6c79d4b03e1445f5f8430831c191132ec845
mira97ali <140742998+mira97ali@users.noreply.github.com>
2023-10-17 00:08:28 +0300

    Update printf_integer.c

--- printf_integer.c
+++ printf_integer.c
@@ -1,42 +1,45 @@
+#include "main.h"
+
 /**
- * print_int - Print int
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * printf_integer - prints intiger number
+ * @args: number arguements
+ * @printed: the printed characters
+ * Return: printed charcaters
  */
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+
+int printf_integer(va_list args, int printed)
 {
-	int i = BUFF_SIZE - 2;
-	int is_negative = 0;
-	long int n = va_arg(types, long int);
-	unsigned long int num;
+	int num = va_arg(args, int);
+	int digits = 0;
+	int temp = num;
+	int digit;
 
-	n = convert_size_number(n, size);
-
-	if (n == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-	num = (unsigned long int)n;
-
-	if (n < 0)
+	if (num < 0)
 	{
-		num = (unsigned long int)((-1) * n);
-		is_negative = 1;
+		printed += _putchar('-');
+		num = -num;
+
+		temp = num;
 	}
 
-	while (num > 0)
+	do {
+		digits++;
+		temp /= 10;
+	} while (temp != 0);
+
+	while (digits > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
+		int pow10 = 1;
+		int i;
+
+		for (i = 1; i < digits; i++)
+		{
+			pow10 *= 10;
+		}
+		digit = num / pow10;
+		printed += _putchar(digit + '0');
+		num -= digit * pow10;
+		digits--;
 	}
-
-	i++;
-
-	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+	return (printed);
 }
