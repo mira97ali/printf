fba49096ce225e02e9f435f98f7ff1ffeaa9bc28
mira97ali <140742998+mira97ali@users.noreply.github.com>
2023-10-17 00:40:22 +0300

    Update main.h

--- main.h
+++ main.h
@@ -54,6 +54,8 @@ int get_size(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size);
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
 int write_number(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size);
 int write_num(int ind, char buffer[],
