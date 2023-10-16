73fff2eea3d548da15d5185d64b1f12a273e0572
BahjaIsmail <140181564+BahjaIsmail@users.noreply.github.com>
2023-10-16 05:40:35 +0300

    Update printf_string.c

--- printf_string.c
+++ printf_string.c
@@ -13,7 +13,7 @@
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int length = 0, i;
+	int length = 0, v;
 	char *str = va_arg(types, char *);
 
 	UNUSED(buffer);
