5bce5a59830b4234688c7a627d14d5577b1cf835
mira97ali <140742998+mira97ali@users.noreply.github.com>
2023-10-17 00:31:18 +0300

    Update main.h

--- main.h
+++ main.h
@@ -54,8 +54,16 @@ int get_size(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size);
-int handle_write_char(char c, char buffer[],
+int write_number(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size);
+int write_num(int ind, char buffer[],
+	int flags, int width, int prec,
+	int length, char padd, char extra_c);
+int write_unsgnd(int is_negative, int ind,
+	char buffer[],
+	int flags, int width, int precision, int size);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
 int is_printable(char c);
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
@@ -84,13 +92,6 @@ int print_int(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int write_number(int is_negative, int ind, char buffer[],
-	int flags, int width, int precision, int size);
-int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
-int write_unsgnd(int is_negative, int ind,
-	char buffer[],
 	int flags, int width, int precision, int size);
 
 #endif
